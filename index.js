06cc8ef408539f5137a33055526673d133369db9
fernando <fernandorestrepo@hotmail.es>
2025-06-20 21:54:58 -0500

    agrege port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import api from "./api.js"
 
 
-api.listen(process.env.PUERTO,()=>{
-   console.log("servidor en puerto 4000")
-})
+const PORT = process.env.PORT || 4000;
+api.listen(PORT, () => {
+   console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
